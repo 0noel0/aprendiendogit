5264bbeae3be4c0f6c6b02ce7d451502e28fdb49
omarzinga <omarzinga@gmail.com>
2015-04-01 11:54:44 -0600

    Cambio de tituto y muchas cosas mas

--- index.php
+++ index.php
@@ -2,10 +2,13 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
-	<title>Aprendiedo git</title>
+	<title>Aprendiedo git con github</title>
 </head>
 <body>
 	<h1>REPOSITORIO INICIAL</h1>
+	<h3>Hola git init </h3>
+	<p>Aprendiendo he instalando git hub</p>
+
 	
 </body>
 </html>
