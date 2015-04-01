90169e88145d393a2ec415bd0d036db1b2ba5473
Noel Hidalgo <nhidalgoca@gmail.com>
2015-04-01 12:07:06 -0600

    cambios retornados

--- index.php
+++ index.php
@@ -2,11 +2,14 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
-	<title>Aprendiedo git Noel modificacion</title>
+	<title>Aprendiedo git con github</title>
+
 </head>
 <body>
 	<h1>REPOSITORIO INICIAL</h1>
 	<img src="http://i.tuselva.com/2015/01/20-datos-curiosos-que-no-sab%C3%ADas-de-los-gatos.jpg">
+	<h3>Hola git init </h3>
+	<p>Aprendiendo he instalando git hub</p>
 	
 </body>
 </html>
