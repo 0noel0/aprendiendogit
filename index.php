44ad9999beb9afeffefc50c2d6aff155f71ce11f
Noel Hidalgo <nhidalgoca@gmail.com>
2015-04-01 12:45:51 -0600

    bootstrap agregado con gato redondo

--- index.php
+++ index.php
@@ -3,15 +3,18 @@
 <head>
 	<meta charset="UTF-8">
 	<title>Aprendiedo git con github</title>
-
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <script src="js/bootstrap.min.js"	></script>
 </head>
 <body>
 	<h1>REPOSITORIO INICIAL</h1>
-	<img src="http://i.tuselva.com/2015/01/20-datos-curiosos-que-no-sab%C3%ADas-de-los-gatos.jpg">
+	<img class="img-responsive img-circle" width="600" height="200" src="http://i.tuselva.com/2015/01/20-datos-curiosos-que-no-sab%C3%ADas-de-los-gatos.jpg">
 	<h3>Hola git init </h3>
 
 	<p>Aprendiendo he instalando git hub en un servidor personal en la nube</p>
-
+	
 
 
 	
