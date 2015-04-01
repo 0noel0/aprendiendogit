2d29683bea634ad6499eddd8c7b9fe5bd84b81b6
omarzinga <omarzinga@gmail.com>
2015-04-01 12:22:45 -0600

    conexion

---
+++ conexion/conexion.php
@@ -0,0 +1,6 @@
+<?php 
+$conexion = mysql_connect('127.0.0.1', 'root', '1001');
+
+
+
+?>
--- index.php
+++ index.php
@@ -7,7 +7,7 @@
 <body>
 	<h1>REPOSITORIO INICIAL</h1>
 	<h3>Hola git init </h3>
-	<p>Aprendiendo he instalando git hub</p>
+	<p>Aprendiendo he instalando git hub en un servidor personal en la nube</p>
 
 	
 </body>
